6e85487ef0bc27002cfe68e990eac1946e1a3997
Gabriel Dahia <gdahia@gmail.com>
2018-04-17 17:43:02 -0300

    Fix non drawable (but unknown) lab01 ex error

--- lab/ex01/index.js
+++ lab/ex01/index.js
@@ -1,20 +1,16 @@
 function init() {
-  scene = new THREE.Scene();
+  var scene = new THREE.Scene();
 
-  renderer = new THREE.WebGLRenderer();
-
+  var renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(new THREE.Color(0, 0, 0));
-
-  renderer.setSize(750, 750);
-
+  renderer.setSize(500, 500);
   document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
-  camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, -1, 1);
-
+  var camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
   scene.add(camera);
 
-  var verticesPerRow = 100;
-  var verticesPerCol = 100;
+  var verticesPerRow = 50;
+  var verticesPerCol = 50;
   var dropletGeometry = getDropletVertices(verticesPerRow, verticesPerCol);
   dropletGeometry =
       getDropletFaces(dropletGeometry, verticesPerRow, verticesPerCol);
@@ -26,18 +22,26 @@ function init() {
   });
 
   var dropletMesh = new THREE.Mesh(dropletGeometry, dropletMaterial);
-
   scene.add(dropletMesh);
 
-  renderer.clear();
+  // var animate = function() {
+  // requestAnimationFrame(animate);
+
+  // dropletMesh.rotation.x += 0.01;
+  // dropletMesh.rotation.y += 0.01;
+
+  // renderer.render(scene, camera);
+  //};
+
+  // animate();
   renderer.render(scene, camera);
 }
 
 function getDropletVertices(verticesPerRow, verticesPerColumn) {
   var dropletGeometry = new THREE.Geometry();
 
-  for (theta = 0; theta <= Math.PI; theta += Math.PI / verticesPerColumn)
-    for (omega = 0; omega <= 2 * Math.PI; omega += 2 * Math.PI / verticesPerRow)
+  for (omega = 0; omega <= 2 * Math.PI; omega += 2 * Math.PI / verticesPerRow)
+    for (theta = 0; theta <= Math.PI; theta += Math.PI / verticesPerColumn)
       dropletGeometry.vertices.push(
           new THREE.Vector3(
               0.5 * (1 - Math.cos(theta)) * Math.sin(theta) * Math.cos(omega),
@@ -48,17 +52,8 @@ function getDropletVertices(verticesPerRow, verticesPerColumn) {
 }
 
 function getDropletFaces(dropletGeometry, verticesPerRow, verticesPerColumn) {
-  for (i = 0; i < dropletGeometry.vertices.length; i++) {
-    row = parseInt(i / verticesPerRow);
-    col = i % verticesPerRow;
-    right_neighbor_index =
-        (row * verticesPerRow + ((col + 1) % verticesPerRow)) %
-        dropletGeometry.vertices.length;
-    down_neighbor_index =
-        ((row + 1) * verticesPerRow + col) % dropletGeometry.vertices.length;
-    dropletGeometry.faces.push(
-        new THREE.Face3(i, right_neighbor_index, down_neighbor_index));
-  }
+  for (i = 0; i < dropletGeometry.vertices.length - verticesPerRow - 1; i++)
+    dropletGeometry.faces.push(new THREE.Face3(i, i + 1, i + verticesPerRow));
 
   return dropletGeometry;
 }
