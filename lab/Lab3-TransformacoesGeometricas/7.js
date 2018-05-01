b8faea1e982b084416c48f35d9734d28eb2755df
Gabriel Dahia <gdahia@gmail.com>
2018-05-01 20:36:46 -0300

    Add lab02 training ex07

---
+++ lab/Lab3-TransformacoesGeometricas/7.js
@@ -0,0 +1,110 @@
+var scene = null;
+var renderer = null;
+var camera = null;
+var earth = null;
+var moon = null;
+var sun = null;
+var day = 0.0;
+var year = 0.0;
+var month = 0.0;
+
+function init() {
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer();
+
+  renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
+  renderer.setSize(500, 500);
+
+  document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
+  camera = new THREE.OrthographicCamera(-1.0, 1.0, 1.0, -1.0, -1.0, 1.0);
+  scene.add(camera);
+
+  // Eixo do Sol
+  var sAxis = new THREE.AxisHelper(0.6);
+
+  // Sol
+  var sphereGeometry = new THREE.SphereGeometry(0.4, 20, 20);
+  var sphereMat =
+      new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true});
+  sun = new THREE.Mesh(sphereGeometry, sphereMat);
+  sun.add(sAxis);
+  scene.add(sun);
+
+  // Eixo da Terra
+  var tAxis = new THREE.AxisHelper(0.15);
+
+  // Eixo da Lua
+  var lAxis = new THREE.AxisHelper(0.04);
+
+  // Lua
+  sphereGeometry = new THREE.SphereGeometry(0.03, 10, 10);
+  sphereMat = new THREE.MeshBasicMaterial({color: 0xaaaaaa, wireframe: true});
+  moon = new THREE.Mesh(sphereGeometry, sphereMat);
+  moon.add(lAxis);
+
+  // Terra
+  sphereGeometry = new THREE.SphereGeometry(0.1, 20, 20);
+  sphereMat = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true});
+  earth = new THREE.Mesh(sphereGeometry, sphereMat);
+  earthGroup = new THREE.Object3D();
+  earth.add(tAxis);
+  earthGroup.add(moon);
+  earthGroup.add(earth);
+  scene.add(earthGroup);
+
+  renderer.clear();
+  render();
+};
+
+function render() {
+  var earthIndMat = new THREE.Matrix4();
+  var earthMat = new THREE.Matrix4();
+  var moonMat = new THREE.Matrix4();
+
+  day += 0.07;
+  year += 0.01;
+  month += 0.04;
+
+  // sun does not move, so sun transfs were removed
+
+  // earth only x rotation
+  earth.matrix.copy(earthIndMat);
+  earthIndMat.makeRotationX(day);
+  earth.applyMatrix(earthIndMat);
+
+  // earth transfs
+  earthGroup.matrix.copy(earthMat);
+
+  // rotate on own axis
+  earthMat.makeRotationY(day);
+  earthGroup.applyMatrix(earthMat);
+
+  // distance from sun
+  earthMat.makeTranslation(0.7, 0, 0);
+  earthGroup.applyMatrix(earthMat);
+
+  // rotate around sun
+  earthMat.makeRotationY(year);
+  earthGroup.applyMatrix(earthMat);
+
+  earthGroup.updateMatrix();
+
+  // moon transfs
+  moon.matrix.copy(moonMat);
+
+  // moon-earth distance
+  moonMat.makeTranslation(0.15, 0, 0);
+  moon.applyMatrix(moonMat);
+
+  // moon-earth rotation
+  moonMat.makeRotationY(day);
+  moon.applyMatrix(moonMat);
+
+  moon.updateMatrix();
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
